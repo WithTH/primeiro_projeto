d5395a2fb7bc75a89043bdae5ee88618927f7a6a
Matheus Oliveira <matheusolive.200227@gmail.com>
2020-08-19 16:06:44 -0300

    Adicionado o meu primeiro programa do VS feito em C++.

---
+++ teste.cpp
@@ -0,0 +1,12 @@
+﻿// teste.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	printf("This is the my first file upload on GitHub");
+	return 0;
+}
